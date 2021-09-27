96ac7935eac60883c6aeb3ee76e5d5185c8402cc
Bruno Pereira dos Santos <brunosanctus@outlook.com>
2021-09-27 13:50:02 -0300

    primeiro commit

---
+++ src/view/Principal.java
@@ -0,0 +1,47 @@
+package view;
+
+import model.Fila;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Fila f = new Fila();
+		boolean vazia = f.isEmpty();
+		System.out.println(vazia);
+		
+		for (int i = 0 ; i < 10 ; i++) {
+			int rnd = (int)((Math.random() * 100) + 1);
+			f.insert(rnd);
+		}
+		System.out.println(f.toString());
+		
+		try {
+			int valor = f.get(3);
+			System.out.println("Dado da posição 3: "+valor);
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+		
+		vazia = f.isEmpty();
+		System.out.println("Vazia: "+vazia);
+		
+		int tamanho = f.size();
+		System.out.println("Tamanho da fila: "+tamanho);
+		
+		try {
+			f.list();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		try {
+			int removido = f.remove();
+			removido = f.remove();
+			f.list();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
